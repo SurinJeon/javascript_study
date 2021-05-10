8449b828ade91c3762cdbacb88fa2933b69d5045
SurinJeon <surin0901@naver.com>
2021-05-10 12:46:16 +0900

    seperate javascript source on js file

--- WebContent/tip05/js/tip05.js
+++ WebContent/tip05/js/tip05.js
@@ -1,3 +1,10 @@
-/**
- * 
- */
+$(function() {
+	$(".size span, .color span").click(function() {
+		$(this).addClass("active");
+		$(this).siblings().removeClass("active"); /* 원래 적용되어 있던건 다 사라지게 */
+	});
+
+	$(".like").click(function() {
+		$(this).toggleClass("active");
+	});
+});
